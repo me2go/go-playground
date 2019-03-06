f5f3a9c45d7bc56b29695a158f185c208ee2d0ac
shuaibc1 <shuaibc1@lenovo.com>
2019-03-06 13:46:10 +0800

    modify main.go

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
-import "github.com/me2go/go-playground/sync/atomic"
+import "github.com/me2go/go-playground/sync"
 
 func main() {
-	atomic.Load64Align()
+	//atomic.Load64Align()
+	sync.ConcurrentReadAndWrite()
 }
